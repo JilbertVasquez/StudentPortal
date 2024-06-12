34c9e106be7f49f4a5ae309c39b208dd91720622
JilbertVasquez <jilbertvasquez@gmail.com>
2024-06-12 12:13:58 +0800

    main

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href='https://fonts.googleapis.com/css?family=Nunito Sans' rel='stylesheet'>
     <link href="index.css" rel="stylesheet">
     <title>Student Portal</title>
 </head>
@@ -13,12 +14,20 @@
 
         </div>
         <div class="container3 bg-danger">
-
+        <h3 class=" head d-flex justify-content-center align-items-center text-uppercase text-center mt-2">Student Portal Login</h3>
         </div>
-        <div class="container4 bg-primary d-flex bg">
-
+        <div class="container4 bg-primary d-flex justify-content-center align-items-center" >
+            <div class="inner-div d-flex bg-danger rounded flex-column">
+                <h3 class=" login-label text-uppercase">login</h3>
+                    <form action="" class="d-flex flex-column justify-content-center align-items-center">
+                        <input type="username" class="username-input rounded" placeholder="username" name="username">
+                        <input type="pwd" class="pwd-input rounded" placeholder="password" name="pwd">
+                        <button type="submit" class="btn btn-light text-uppercase">login</button>
+                    </form>
+                    <p class="forgot text-uppercase">forgot password? click here</p>
+            </div>
         </div>
-        
+        <!-- <h1>hello world</h1> -->
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
