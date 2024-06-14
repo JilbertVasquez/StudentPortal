83d15a0383037b8dd9ae88a401ae298f063afdfa
JilbertVasquez <jilbertvasquez@gmail.com>
2024-06-14 18:52:11 +0800

    drive section

--- userdrive.php
+++ userdrive.php
@@ -7,14 +7,20 @@
     <link href='https://fonts.googleapis.com/css?family=Nunito Sans' rel='stylesheet'>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.1.1/css/boxicons.min.css" rel="stylesheet">
     <link href="userdrive.css" rel="stylesheet">
+    <script src="includes\changesection.js"></script>
     <title>User Drive</title>
 </head>
 <body >
     <section class="section-list bg-primary">
-
+        <header class="head d-flex">
+            <img class="alien-pic mt-3 px-4" src="photos\aliens.png" alt="alien">
+            <h1 class="university text-uppercase d-flex justify-content-center align-items-center mt-2">Alien University</h1>
+        </header>
     </section>
     <section class="mydrive bg-danger">
-
+        <header>
+            
+        </header>
     </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
